ed204ff54e2dbcea9f2d6d05a9d8d26a82361853
Dylan McGannon <peltium@gmail.com>
2021-03-20 14:47:36 +1100

    look in the x-forwarded-for header

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use std::convert::Infallible;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, HeaderMap};
+use hyper::header::HeaderValue;
 use hyper::server::conn::AddrStream;
 use std::env;
+
+fn check_for_header(headers: &HeaderMap<HeaderValue>, header_key: &str) -> Result<String, ()> {
+  if let Some(client_ip) = headers.get(header_key) {
+    if let Ok(client_ip_str) = client_ip.to_str() {
+      return Ok(client_ip_str.into());
+    }
+  }
+  Err(())
+}
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -13,7 +23,13 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
   let make_svc = make_service_fn(|socket: &AddrStream| {
     let remote_address = socket.remote_addr();
     async move {
-      Ok::<_, Infallible>(service_fn(move |_: Request<Body>| async move {
+      Ok::<_, Infallible>(service_fn(move |request: Request<Body>| async move {
+        let headers = request.headers();
+        if let Ok(client_ip) = check_for_header(headers, "x-forwarded-for") {
+          return Ok::<_, Infallible>(
+            Response::new(Body::from(client_ip))
+          );
+        }
         Ok::<_, Infallible>(
           Response::new(Body::from(remote_address.ip().to_string()))
         )
